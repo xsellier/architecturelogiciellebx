e17b9ff98ed683d189690e412dfcaea1aec55849
pphommar <pphommar@952717c6-cec6-11dd-aa7b-9fdd872f0417>
2008-12-22 12:41:17 +0000

    Modification du framework (refactoring) : passage à une classe de constantes qui contient seulement les variables utilisées un peu partout dans le projet.

---
+++ src/gameframework/game/ConstantValues.java
@@ -0,0 +1,20 @@
+package gameframework.game;
+
+public class ConstantValues {
+	private ConstantValues() {
+	}
+
+	public static final int SPRITE_SIZE = 16;
+
+	public static final int SPRITE_SIZE_X = 29;
+
+	public static final int SPRITE_SIZE_Y = 36;
+
+	public static final int NB_ROWS = 31;
+
+	public static final int NB_COLUMNS = 28;
+
+	public static final int MAX_NUMBER_OF_PLAYER = 4;
+
+	public static final int NUMBER_OF_LIVES = 1;
+}
