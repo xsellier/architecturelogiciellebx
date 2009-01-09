d05b2914cb65ccf9c907e919abb7913e428d649f
pphommar <pphommar@952717c6-cec6-11dd-aa7b-9fdd872f0417>
2009-01-09 16:49:30 +0000

    corollaire rename

--- trunk/src/bomberman/Main.java
+++ trunk/src/bomberman/Main.java
@@ -1,15 +1,14 @@
 package bomberman;
 
-import gameframework.game.GameDefaultImpl;
 import gameframework.game.GameLevel;
 
 import java.util.ArrayList;
 
-import bomberman.game.GameDefaultImplProxy;
+import bomberman.game.GameDefaultImplExt;
 
 public class Main {
 	public static void main(String[] args) {
-		GameDefaultImplProxy g = new GameDefaultImplProxy();
+		GameDefaultImplExt g = new GameDefaultImplExt();
 		ArrayList<GameLevel> levels = new ArrayList<GameLevel>();
 
 		levels.add(new BombermanGameLevel(g));
