29518dc3c4436dab7f40bc1f844137e072a300e1
pphommar <pphommar@952717c6-cec6-11dd-aa7b-9fdd872f0417>
2009-01-17 16:39:47 +0000

    abstraction de la strategie clavier : utilisation du pattern methode de fabrication

---
+++ src/bomberman/base/MoveStrategyKeyboardPlayer2.java
@@ -0,0 +1,27 @@
+package bomberman.base;
+
+import java.awt.event.KeyEvent;
+
+public class MoveStrategyKeyboardPlayer2 implements MoveStrategyKeyboardMethodFactory {
+
+	public int right() {
+		return KeyEvent.VK_RIGHT;
+	}
+	
+	public int left() {
+		return KeyEvent.VK_LEFT;
+	}
+	
+	public int down() {
+		return KeyEvent.VK_DOWN;
+	}
+	
+	public int up() {
+		return KeyEvent.VK_UP;
+	}
+	
+	public int action() {
+		return KeyEvent.VK_NUMPAD0;
+	}
+
+}
